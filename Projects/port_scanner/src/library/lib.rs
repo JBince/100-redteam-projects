0c1fe9b046668bd5d25b0b8375384c00344f5f39
JBince <alexander.philiotis@gmail.com>
2023-03-20 13:32:05 -0500

    Update port scanner text output

--- Projects/port_scanner/src/library/lib.rs
+++ Projects/port_scanner/src/library/lib.rs
@@ -1,6 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{Error, ErrorKind, Read};
 use std::net::{Shutdown, TcpStream};
+use colored::Colorize;
 pub struct Target {
     pub ip: String,
     pub start: u16,
@@ -48,7 +49,7 @@ impl Target {
                     }
                 //If the range is not a single port and it doesn't match any of the other cases, then return the default target
                 } else {
-                    println!("Invalid port range. Using 1-65535");
+                    println!("{}","[!] Invalid port range. Using 1-65535".red().italic());
                     Target::default()
                 }
             }
@@ -57,39 +58,43 @@ impl Target {
 }
 
 pub fn scan(target: Target) {
+    let mut open_ports: Vec<u16> = Vec::new();
     for port in target.start..=target.finish {
         let stream = TcpStream::connect(format!("{}:{}", target.ip, port));
         match stream {
-            Ok(mut stream) => {
-                let mut banner: Vec<u8> = vec![0; 1024];
+            Ok(stream) => {
 
-                let mut rx_bytes = [0u8; 1024];
+                //Non functional code to read a ports banner.
+
+                // let mut banner: Vec<u8> = vec![0; 1024];
+
+                // let mut rx_bytes = [0u8; 1024];
                 
-                println!("Reading bytes");
-                let bytes_read = stream.read(&mut rx_bytes).unwrap();
-                println!("Bytes read");
+                // let bytes_read = stream.read(&mut rx_bytes).unwrap();
 
-                if bytes_read == 0 {
-                    break;
-                }
-                banner.extend_from_slice(&rx_bytes);
+                // if bytes_read == 0 {
+                //     break;
+                // }
+                // banner.extend_from_slice(&rx_bytes);
 
-                String::from_utf8(banner)
-                    .map(|msg| println!("{}", msg))
-                    .map_err(|_| {
-                        Error::new(
-                            ErrorKind::InvalidData,
-                            "Couldn't parse received string as utf8",
-                        )
-                    })
-                    .ok();
+                // String::from_utf8(banner)
+                //     .map(|msg| println!("{}", msg))
+                //     .map_err(|_| {
+                //         Error::new(
+                //             ErrorKind::InvalidData,
+                //             "Couldn't parse received string as utf8",
+                //         )
+                //     })
+                //     .ok();
 
-                println!("{}: Open", port);
+                println!("{} {}: Open","[+]".green(), port);
                 stream.shutdown(Shutdown::Both).unwrap();
+                open_ports.push(port);
             }
             Err(_) => {
-                println!("{}: Closed", port);
+                ();
             }
         }
     }
+    println!("{}{}","[*] Total open Ports: ".green(), open_ports.len());
 }
